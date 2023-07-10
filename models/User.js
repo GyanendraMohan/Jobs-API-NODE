f81873006aeed8325cacf4da142496e098d3ef4c
GyanendraMohan <gmpatel152@gmail.com>
2023-07-10 21:38:15 +0530

    hashing the password using mongoose middleware

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,15 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide password"],
     minlength: 6,
   },
+});
+
+// hashing the password using mongoose middleware
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
