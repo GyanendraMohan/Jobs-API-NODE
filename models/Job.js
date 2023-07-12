c0159464a6678523bc354db263bfedb176d820c1
GyanendraMohan <gmpatel152@gmail.com>
2023-07-12 13:58:49 +0530

    create job model

--- models/Job.js
+++ models/Job.js
@@ -1,1 +1,30 @@
+const { required } = require("joi");
 const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please Provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please Provide company name"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "decline", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please Provide User"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", JobScheema);
